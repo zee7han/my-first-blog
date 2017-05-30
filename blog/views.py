524a8548eaa581eccbd2342e339e25e5c99c1f04
zee7han <zee7han@gmail.com>
2017-05-30 15:46:01 +0530

    That is my fifth commit

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,7 @@ from django.utils import timezone
 # Create your views here.
 def post_list(request):
     Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts':post})
+    return render(request, 'blog/post_list.html', {'posts':posts})
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
